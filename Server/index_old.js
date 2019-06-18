6f0723eae877013528d9d4a6eeec327cfdf0203e
nadavlotan <nadavlotan6@gmail.com>
2019-06-18 23:58:48 +0300

    Final Version For ABC

--- Server/index_old.js
+++ Server/index_old.js
@@ -3,7 +3,6 @@
  * Nadav Lotan
  * MiLAB project
  */
-// some changes
 
 // dialogflow constants
 const dialogflow = require('dialogflow');
@@ -20,9 +19,9 @@ const port = process.env.PORT || 3000;
 // Set user first input interaction
 global.user_input = 'help';
 
+// function startServer() {
 // Geting in active when the user connects to this server.
 io.on('connection', (socket) => {
-  console.log('User has connected!')
   projectId = 'abc-yryhlp'
   // A unique identifier for the given session
   const sessionId = uuid.v4();
@@ -32,8 +31,6 @@ io.on('connection', (socket) => {
 
   runSample()
 
-  let dict = [];
-  let index = 0;
   /**
    * Send a query to the dialogflow agent, and return the query result.
    * @param {string} projectId The project to be used
@@ -53,7 +50,7 @@ io.on('connection', (socket) => {
             text: user_input,
             // The language used by the client (en-US)
             languageCode: 'en-US',
-          },  
+          },
         },
       };
 
@@ -61,7 +58,7 @@ io.on('connection', (socket) => {
       let responses = await sessionClient.detectIntent(request);
       let result = responses[0].queryResult;
 
-      console.log(`${result.fulfillmentText}`);
+      console.log(`${result.fulfillmentText}`); 
 
       socket.emit('serverMessage', `${result.fulfillmentText}`);
       console.log('serverMessage: ' + `${result.fulfillmentText}`);
@@ -72,46 +69,27 @@ io.on('connection', (socket) => {
       } else {
         console.log(` No intent matched.`);
       }
-      console.log("A text fulfillment")
-      console.log(result.fulfillmentText)
 
-      // catching if there is a dialogflow answer that doesn't ask a question.
-      if (`${result.fulfillmentText}` == "Ok, I'm now checking your pulse, please remain still for a few seconds.") {
-        console.log("Server text:" + user_input)
-        global.user_input = "blood test finished"
-        // need to insert a timeout of 10 sec for the blood pressure test.
-        console.log("Server text:" + user_input)
-        setTimeout(function () {
-          runSample();
-        }, 5000);
 
-      } else if (`${result.fulfillmentText}` == "Pulse rate successfully checked") {
-        global.user_input = "intro to question"
-        setTimeout(function () {
-          runSample();
-        }, 3200);
-
-      } else if (`${result.fulfillmentText}` == "To help me define your injury ,answer the next 5 questions") {
-        global.user_input = "month"
-        setTimeout(function () {
-          runSample();
-        }, 2000);
-      }
-
+      // // catching if there is a dialogflow answer that doesn't ask a question.
+      // if (`${result.fulfillmentText}` == "Great! I'm now checking your blood pressure, please remain still for a few seconds.") {
+      //   user_input = "Blood test finished"
+      //   // need to insert a timeout of 10 sec for the blood pressure test.
+      // } else if (`${result.fulfillmentText}` == "Blood successfully checked!") {
+      //   user_input = "intro to question"
+      // } else if (`${result.fulfillmentText}` == "To help me define your injury, answer the next 4 questions.") {
+      //   user_input = "month"
+      // } else {
+      //   // user_input = readlineSync.question('');
+      // }
     } catch (err) {
       console.log(err)
     }
   }
+  // socket.emit('serverMessage', "Welcome to ABC. How can I help you?");
 
   // Tested if connected 
   console.log('one user connected ' + socket.id);
-
-  socket.on('getData', (message) => {
-    console.log("GetData occured!!!!! " + dict);
-    socket.emit('receivingData', dict);
-    dict = [];
-    index = 0;
-  })
 
   // when the client emits 'userMessage' this executses.
   socket.on('userMessage', (message) => {
@@ -119,30 +97,34 @@ io.on('connection', (socket) => {
     global.user_input = message['userMessage']
     console.log(message['userMessage'])
 
-    dict.push({
-      key: index,
-      value: message['userMessage']
-    });
+    runSample()
+  })
 
-    index += 1;
-    console.log(dict);
-
-    if (message['userMessage'] == "restart") {
-      dict = [];
-      index = 0;
-    }
+   // when recieving a closeConnection event (onDestroy), restart dialogflow session
+   socket.on('closeConnection', ()=> {
+    global.user_input = 'restart'
     runSample()
   })
 
   // In case a user has disconnected
   socket.on('disconnect', () => {
-    global.user_input = "restart"
-    runSample()
     console.log('one user disconnected ' + socket.id);
   })
 });
 
+// }
 // Set a server connection and listen on 
 server.listen(port, function () {
-  console.log("listening on port 3000");
+  console.log("listening on port 5000");
 });
+
+// app.get('/', function (request, response) {
+
+//   startServer();
+// });
+
+// app.set('port', (5000 || process.env.PORT));
+
+// app.listen(app.get('port'), function () {
+//   console.log('Node app is running on port', app.get('port'))
+// });
